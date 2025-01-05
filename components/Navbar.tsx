a34e059af571435855e35b107cc7075ecd0c1276
monami_nandy <monaminandy16@gmail.com>
2025-01-05 12:58:26 +0530

    Initial commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link"
+import Image  from "next/image"
+import { NAV_LINKS } from "@/constants"
+import Button from "./Button"
+
+
+const Navbar = () => {
+  return (
+    <nav className="flexBetween max-container padding-container relative z-30 py-5">
+        <Link href="/">
+            <Image src="/wotwlogo.svg" alt="logo" width={200} height={20}/>
+            </Link>
+        <ul className="hidden h-full gap-12 lg:flex">
+            {NAV_LINKS.map((link)=>(
+                <Link href={link.href} key={link.key} className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold">
+                    {link.label}
+                </Link>
+            ))}
+        </ul>
+        <div className="hidden lg:flex">
+            <Button 
+            type="button"
+            title="Login"
+            icon="/user.svg"
+            variant="btn_dark_green"
+            />
+        </div>
+        
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ constants/index.js
@@ -0,0 +1,8 @@
+// NAVIGATION
+export const NAV_LINKS = [
+    { href: '/', key: 'home', label: 'Home' },
+    { href: '/', key: 'how_wotw_work', label: 'How WOTW Work?' },
+    { href: '/', key: 'services', label: 'Services' },
+    { href: '/', key: 'pricing ', label: 'Pricing ' },
+    { href: '/', key: 'contact_us', label: 'Contact Us' },
+  ];
